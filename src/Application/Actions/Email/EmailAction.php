79fdf7d4508ae181265fc2bfb88ff67434712974
Juan Diego Zuniga Arias <juandiegoster@gmail.com>
2023-08-15 22:24:42 -0600

    adding cors

--- src/Application/Actions/Email/EmailAction.php
+++ src/Application/Actions/Email/EmailAction.php
@@ -5,6 +5,7 @@ namespace App\Application\Actions\Email;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Tuupola\Middleware\CorsMiddleware;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -13,6 +14,19 @@ class EmailAction
 {
     public function __invoke(Request $request, Response $response): Response
     {
+        // Configura el middleware CORS
+        $cors = new CorsMiddleware([
+            "origin" => ["*"], // Cambia esto con los dominios permitidos
+            "methods" => ["GET", "POST", "OPTIONS"],
+            "headers.allow" => ["Authorization", "Content-Type"],
+            "headers.expose" => [],
+            "credentials" => true,
+            "cache" => 0,
+        ]);
+
+        // Aplica el middleware CORS a la solicitud
+        $request = $cors->process($request);
+
         $data = $request->getParsedBody();
 
         // Configuración de PHPMailer
@@ -40,7 +54,7 @@ class EmailAction
 
             $response->getBody()->write(json_encode(['mensaje' => 'Correo enviado']));
         } catch (Exception $e) {
-            $response->getBody()->write(json_encode(['error' => 'Error al enviar el correo']));
+            $response->getBody()->write(json_encode(['error' => 'Error al enviar el correos'. $e]));
         }
 
         return $response
